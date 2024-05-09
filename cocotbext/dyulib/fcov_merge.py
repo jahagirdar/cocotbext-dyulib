80a4ef10bc365490a77f4e33ab5cca7611c8f87f
Vijayvithal Jahagirdar <jvs@dyumnin.com>
2024-05-09 20:28:32 +0530

    Checkin of functional coverage merge script.

---
+++ cocotbext/dyulib/fcov_merge.py
@@ -0,0 +1,33 @@
+from cocotb_coverage.coverage import merge_coverage, coverage_db
+from .dyutools import find_all
+import logging
+import xml.etree.ElementTree as ET
+import click
+
+
+@click.command()
+@click.option('--path', prompt="The path from where to search")
+@click.option('--filename', prompt="The name of the coverage xml file")
+def fcov_merge(path='./', filename='coverage.xml'):
+    log = logging.getLogger("merge coverage")
+    all_files = find_all(filename, path)
+    log.info(f"Coverage files found {all_files}")
+    merge_coverage(log.info, "functional_coverage.xml", *all_files)
+    root = ET.parse("functional_coverage.xml")
+    top = root.getroot()
+    section = ET.Element("section")
+    field = ET.SubElement(section, "field")
+    field.set("name", top.attrib['abs_name'])
+    field.set("value", top.attrib['cover_percentage'])
+    for e in root.findall(".//*[@coverage]"):
+        field = ET.SubElement(section, "field")
+        field.set("name", e.attrib['abs_name'])
+        field.set("value", e.attrib['cover_percentage'])
+    ET.ElementTree(element=section).write('fcov_report.xml')
+
+    coverage_db.report_coverage(log.info)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    fcov_merge()
